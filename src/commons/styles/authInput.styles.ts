80219650127e4022ff61a1cd57de9e5b8d2ec35d
devgyuri <gksmf910@gmail.com>
2024-07-28 19:00:44 +0900

    style: login form style

---
+++ src/commons/styles/authInput.styles.ts
@@ -0,0 +1,89 @@
+import styled from "@emotion/styled";
+
+interface IInputProps {
+  isError: boolean;
+}
+
+export const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Form = styled.form`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InputItemWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 50px;
+  padding-left: 16px;
+  padding-right: 40px;
+  margin-bottom: 10px;
+
+  border: ${(props: IInputProps) =>
+    props.isError ? "1px solid var(--error-color)" : "1px solid var(--gray)"};
+  border-radius: 8px;
+
+  font-size: 16px;
+
+  &::placeholder {
+    font-weight: 300;
+    color: var(--light-gray);
+  }
+
+  &:focus {
+    outline: none;
+    border-color: var(--primary-color);
+  }
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  height: 50px;
+  background-color: var(--primary-color);
+  color: var(--white);
+  border: none;
+  border-radius: 8px;
+  font-size: 16px;
+  font-weight: 500;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const InverseButton = styled.button`
+  width: 100%;
+  height: 50px;
+  background-color: var(--white);
+  color: var(--primary-color);
+  border: 1px solid var(--primary-color);
+  border-radius: 8px;
+  font-size: 16px;
+  font-weight: 500;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const ErrorMessage = styled.div`
+  margin-bottom: 10px;
+  font-size: 14px;
+  font-weight: 500;
+  color: var(--error-color);
+`;
--- src/components/units/loginForm/LoginForm.index.tsx
+++ src/components/units/loginForm/LoginForm.index.tsx
@@ -1,10 +1,11 @@
 import { useCookies } from "react-cookie";
 import { useLogin } from "../../../commons/hooks/useLogin";
-import * as S from "./LoginForm.styles";
+import * as S from "../../../commons/styles/authInput.styles";
 import { useForm } from "react-hook-form";
 import { AUTH_FORM_REGEX } from "../../../commons/constant/authFormRegex";
 import { ILoginInput } from "../../../commons/types/auth/loginInput";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function LoginForm(): JSX.Element {
   const router = useRouter();
@@ -53,40 +54,44 @@ export default function LoginForm(): JSX.Element {
     <>
       <S.Wrapper>
         <S.Form onSubmit={handleSubmit(onSubmit)}>
-          <S.Label>이메일</S.Label>
-          <S.EmailInput
-            type="email"
-            placeholder="이메일을 입력해 주세요."
-            aria-invalid={errors.email ? "true" : "false"}
-            {...register("email", {
-              required: "이메일을 입력해 주세요.",
-              pattern: {
-                value: AUTH_FORM_REGEX.email,
-                message: "이메일 형식을 다시 확인해 주세요.",
-              },
-            })}
-          />
-          <S.Label>비밀번호</S.Label>
-          <S.PasswordInput
-            type="password"
-            placeholder="비밀번호를 입력해 주세요."
-            aria-invalid={errors.password ? "true" : "false"}
-            {...register("password", {
-              required: "비밀번호를 입력해 주세요.",
-              pattern: {
-                value: AUTH_FORM_REGEX.password,
-                message: "비밀번호 형식을 다시 확인해 주세요.",
-              },
-            })}
-          />
-          {errors.email ? (
-            <S.ErrorMessage>{errors.email.message}</S.ErrorMessage>
-          ) : errors.password ? (
-            <S.ErrorMessage>{errors.password.message}</S.ErrorMessage>
-          ) : (
-            errors.root && (
-              <S.ErrorMessage>{errors.root.message}</S.ErrorMessage>
-            )
+          <S.InputWrapper>
+            <S.Input
+              type="email"
+              placeholder="이메일을 입력해 주세요."
+              aria-invalid={errors.email ? "true" : "false"}
+              {...register("email", {
+                required: "이메일을 입력해 주세요.",
+                pattern: {
+                  value: AUTH_FORM_REGEX.email,
+                  message: "이메일 형식을 다시 확인해 주세요.",
+                },
+              })}
+              isError={!!errors.email}
+            />
+            {errors.email && (
+              <S.ErrorMessage>{errors.email.message}</S.ErrorMessage>
+            )}
+          </S.InputWrapper>
+          <S.InputWrapper>
+            <S.Input
+              type="password"
+              placeholder="비밀번호를 입력해 주세요."
+              aria-invalid={errors.password ? "true" : "false"}
+              {...register("password", {
+                required: "비밀번호를 입력해 주세요.",
+                pattern: {
+                  value: AUTH_FORM_REGEX.password,
+                  message: "비밀번호 형식을 다시 확인해 주세요.",
+                },
+              })}
+              isError={!!errors.password}
+            />
+            {errors.password && (
+              <S.ErrorMessage>{errors.password.message}</S.ErrorMessage>
+            )}
+          </S.InputWrapper>
+          {errors.root && (
+            <S.ErrorMessage>{errors.root.message}</S.ErrorMessage>
           )}
           <S.Button type="submit">로그인</S.Button>
         </S.Form>
